323206cb06863aab24ddfdc1da79ee12a1add9c8
Hines Vaughan III <hvaughan3@gmail.com>
2018-01-30 22:02:16 -0600

    Using API calls for project selection page

--- src/Connect/Connect/ViewModels/ProjectsViewModel.cs
+++ src/Connect/Connect/ViewModels/ProjectsViewModel.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Connect.Helpers;
 using Connect.Models;
 using Xamarin.Forms;
 
@@ -118,83 +120,83 @@ namespace Connect.ViewModels {
 
 			IsBusy = true;
 
-#if DEBUG
-		    await Task.FromResult(0);
+//#if DEBUG
+//		    await Task.FromResult(0);
 
-            Projects.Add(new Project {
-                customerName = "Generic Customer",
-                owningBu     = "9500 Biometrics",
-                phase                  = 4,
-                primaryIndication      = "Dry Eye",
-                primaryTherapeuticArea = "Psychiatry",
-                projectDirector = "Sally Smith",
-                projectId = "Project 2",
-                protocolId = "9083-E1-ES3"
-            });
+//            Projects.Add(new Project {
+//                customerName = "Generic Customer",
+//                owningBu     = "9500 Biometrics",
+//                phase                  = 4,
+//                primaryIndication      = "Dry Eye",
+//                primaryTherapeuticArea = "Psychiatry",
+//                projectDirector = "Sally Smith",
+//                projectId = "Project 2",
+//                protocolId = "9083-E1-ES3"
+//            });
 
-            Projects.Add(new Project {
-                customerName = "Generic Customer",
-                owningBu = "2000 Oncology",
-                phase = 2,
-                primaryIndication = "Hodgkin Lymphoma",
-                primaryTherapeuticArea = "Oncology",
-                projectDirector = "Lisa Jones",
-                projectId = "Project 1",
-                protocolId = "7710-TM89-Y0"
-            });
+//            Projects.Add(new Project {
+//                customerName = "Generic Customer",
+//                owningBu = "2000 Oncology",
+//                phase = 2,
+//                primaryIndication = "Hodgkin Lymphoma",
+//                primaryTherapeuticArea = "Oncology",
+//                projectDirector = "Lisa Jones",
+//                projectId = "Project 1",
+//                protocolId = "7710-TM89-Y0"
+//            });
 
-            Projects.Add(new Project {
-                customerName = "Generic Customer",
-                owningBu = "4000 General Medicine",
-                phase = 4,
-                primaryIndication = "Acne",
-                primaryTherapeuticArea = "Dermatology",
-                projectDirector = "James White",
-                projectId = "Project 3",
-                protocolId = "5404-Y5TT-U1"
-            });
+//            Projects.Add(new Project {
+//                customerName = "Generic Customer",
+//                owningBu = "4000 General Medicine",
+//                phase = 4,
+//                primaryIndication = "Acne",
+//                primaryTherapeuticArea = "Dermatology",
+//                projectDirector = "James White",
+//                projectId = "Project 3",
+//                protocolId = "5404-Y5TT-U1"
+//            });
 
-            Projects.Add(new Project {
-                customerName = "Generic Customer",
-                owningBu = "1000 Central Nervous System (CNS)",
-                phase = 4,
-                primaryIndication = "Eyelid Spasms",
-                primaryTherapeuticArea = "Neurology",
-                projectDirector = "Tom Johnson",
-                projectId = "Project 4",
-                protocolId = "8008-1GG6-P3"
-            });
-#endif
+//            Projects.Add(new Project {
+//                customerName = "Generic Customer",
+//                owningBu = "1000 Central Nervous System (CNS)",
+//                phase = 4,
+//                primaryIndication = "Eyelid Spasms",
+//                primaryTherapeuticArea = "Neurology",
+//                projectDirector = "Tom Johnson",
+//                projectId = "Project 4",
+//                protocolId = "8008-1GG6-P3"
+//            });
+//#else
+            try {
+                string url = "https://ecs.incresearch.com/ECS/mobile/project";
+
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Authorization", App.AuthKey);
+
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if(response.IsSuccessStatusCode) {
+                    string content = await response.Content.ReadAsStringAsync();
+                    List<Project> projects = Utility.DeserializeResponse<List<Project>>(content, "data/projects/projectinfo");
+
+                    Projects.Clear();
+
+                    foreach(Project project in projects) {
+                        Projects.Add(project);
+                    }
+                }
+
+            } catch(Exception ex) {
+                ContentPage page = new ContentPage();
+                await page.DisplayAlert("Error", "Unable to load projects.", "OK");
+            }
+//#endif
 		    DisplayProjects = new ObservableCollection<Project>(Projects);
 
             OnPropertyChanged(nameof(DisplayProjects));
 		    OnPropertyChanged(nameof(ProjectCount));
 
-            //         try {
-            //	string url = "https://ecs.incresearch.com/ECS/mobile/project";
-
-            //	HttpClient client = new HttpClient();
-            //	client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Authorization", App.AuthKey);
-
-            //	HttpResponseMessage response = await client.GetAsync(url);
-
-            //	if (response.IsSuccessStatusCode) {
-            //		string content = await response.Content.ReadAsStringAsync();
-            //		List<Project> projects = Utility.DeserializeResponse<List<Project>>(content, "data/projects/projectinfo");
-
-            //                 Projects.Clear();
-
-            //		foreach (Project project in projects) {
-            //			Projects.Add(project);
-            //		}
-            //	}
-
-            //} catch (Exception ex) {
-            //	ContentPage page = new ContentPage();
-            //	await page.DisplayAlert("Error", "Unable to load projects.", "OK");
-            //}
-
-		    if(App.SelectedProject != null) {
+            if(App.SelectedProject != null) {
 		        Project selectedProject = Projects.FirstOrDefault(proj => proj.projectId == App.SelectedProject.projectId && proj.protocolId == App.SelectedProject.protocolId); //TODO: Ensure this is a good way to determine project equality (are protocolId and/or projectId always unique?)
 
 		        if(selectedProject != null) {
