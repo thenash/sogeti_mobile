cac2d5ef5eea88ad20ac37b80a04321253ef3fc2
Hines Vaughan III <hvaughan3@gmail.com>
2018-03-21 20:57:57 -0500

    Created initial base page to add swipe control to for all inheritors

---
+++ src/Connect/Connect/Pages/BaseDetailPage.cs
@@ -0,0 +1,22 @@
+﻿using Connect.ViewModels;
+using Xamarin.Forms;
+
+namespace Connect.Pages {
+
+    public class BaseDetailPage<T> : ContentPage where T : BaseViewModel, new() {
+
+        private T _viewModel;
+        protected T ViewModel {
+            get => _viewModel ?? (_viewModel = new T());
+            set => _viewModel = value;
+        }
+
+        public BaseDetailPage() {
+            BindingContext = ViewModel;
+        }
+
+        ~BaseDetailPage() {
+            _viewModel = null;
+        }
+    }
+}
